9a62923f3d37f89f63e828ad695a3830c183faa2
novagestionsoftware <novagestionsoftware@gmail.com>
2025-03-13 20:01:38 -0300

    env sticky

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -26,8 +26,10 @@ export default function Layout() {
         className={`flex flex-col w-full min-h-screen pl-16 transition-all duration-300 bg-layout `}
       >
         {environment === 'development' && (
-          <div className="absolute top-0 left-1/2 z-50 p-1 2xl:p-2 text-white font-bold text-xl  bg-red-600 hover:bg-red-700 border-b-2 border-x-2 border-red-800 shadow-2xl rounded-b-md cursor-default -translate-x-1/2 transition">
-            Desarrollo
+          <div className="fixed top-0 left-1/2 z-50 transform -translate-x-1/2">
+            <div className="p-1 2xl:p-2 text-white font-bold text-xl bg-red-600 hover:bg-red-700 border-b-2 border-x-2 border-red-800 shadow-2xl rounded-b-md cursor-default transition">
+              Desarrollo
+            </div>
           </div>
         )}
 
