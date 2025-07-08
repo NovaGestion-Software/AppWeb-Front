f96690df79d888c423bae62eb643ddbc2d8a0bef
NovaGestionSoftware <novagestionsoftware@gmail.com>
2025-07-08 13:58:17 -0300

    modificacion de tipos y validacion en getSucursalButton

--- src/utils/helpers/botonera.tsx
+++ src/utils/helpers/botonera.tsx
@@ -1,11 +1,12 @@
 import { ExcelExportConfig } from "@/types";
 import { BotoneraConfig } from "@/types/ButtonConfig";
+import { ReactNode } from "react";
 import { FaStoreAlt } from "react-icons/fa";
 type BotonSucursalConfig = {
   setVisible: (val: boolean) => void;
   conTexto?: boolean;
   disabled?: boolean;
-  iconSize?: string;
+  iconSize?: string | number;
 };
 
 export function getBotonSucursal({
@@ -14,10 +15,18 @@ export function getBotonSucursal({
   disabled = false,
   iconSize = "text-[0.75rem] v1440:text-base v1536:text-lg v1920:text-xl",
 }: BotonSucursalConfig): BotoneraConfig {
+  let icon: ReactNode;
+
+  if (typeof iconSize === "string") {
+    icon = <FaStoreAlt className={iconSize} />;
+  } else {
+    icon = <FaStoreAlt size={iconSize} />;
+  }
+
   return {
     type: "simple",
     text: conTexto ? "Sucursales" : undefined,
-    icon: <FaStoreAlt className={iconSize}  />,
+    icon,
     onClick: () => setVisible(true),
     color: "blue",
     disabled,
@@ -25,8 +34,6 @@ export function getBotonSucursal({
       "h-6 rounded-md text-xxs  v1440:h-8 v1536:h-9 v1536:px-6 v1536:text-sm v1920:h-9",
   };
 }
-
-
 export function crearExportConfig(
   nombreArchivo: string,
   nombreHoja: string,
