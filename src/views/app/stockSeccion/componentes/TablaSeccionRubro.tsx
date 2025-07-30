1b60c2bef6887071f41da6a08aa55cd97acc5129
novagestionsoftware <novagestionsoftware@gmail.com>
2025-07-30 13:30:46 -0300

    cambio venta seccion rubro

--- src/views/app/stockSeccion/componentes/TablaSeccionRubro.tsx
+++ src/views/app/stockSeccion/componentes/TablaSeccionRubro.tsx
@@ -7,9 +7,8 @@ import { ExtendedColumn } from "@/frontend-resourses/components/Tables/types";
 import { useObtenerProductos } from "../hooks/useObtenerProductos";
 import showAlert from "@/frontend-resourses/utils/showAlert";
 import CheckboxInput from "@/frontend-resourses/components/Inputs/Checkbox";
-import {buscarEnArray, FiltrarItemsTraidos } from "@/frontend-resourses/utils/dataManipulation";
+import {buscarEnArray, FiltrarItemsTraidos, sonArrayIguales } from "@/frontend-resourses/utils/dataManipulation";
 import BusquedaInputs from "@/frontend-resourses/components/Tables/Busqueda/BusquedaInputs";
-import { sonArrayIguales } from "@/frontend-resourses/utils/dataManipulation";
 
 interface TablaSeccionRubroProps {
   data: TablaSecciones[];
